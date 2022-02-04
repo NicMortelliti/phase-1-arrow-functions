881f318b6da0f16b9907a84a3eb4dfb3d3cd36bd
Nicolas Mortelliti <npmortelliti@gmail.com>
2022-02-03 18:18:50 -0800

    commit tree 1d5104a28e90162af262c7a5c4fc0170fd2ab288

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression called divide.
+const divide = function() {
+  // Divides 2000 by 100.
+  // Expect divide (function) to equal 20.
+  return 2000 / 100;
+}
+
+// Arrow function called square.
+// Takes one parameter, multiplies it times itself.
+const square = x => x * x;
+
+// Arrow function called add.
+// Takes two params and adds them together.
+const add = (x, y) => x +y;
